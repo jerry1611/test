5dfd105ae885d99980476d1b5a43302ca541f3d3
SreeTeja <Sree.Palli3@target.com>
2018-09-20 11:49:03 -0500

    Updated state and added log

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }) => {
+const store = createStore((state = { count: 100 }) => {
   return state;
 });
 console.log(store.getState());
+console.log("test");

204d573b40a69da884d2be58b070fde512b8011a
SreeTeja <Sree.Palli3@target.com>
2018-09-20 14:27:38 -0500

    changes to quotes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,11 +1,11 @@
-import React from "react";
-import NotFoundPage from "../components/NotFoundPage";
-import Dashboard from "../components/Dashboard";
-import EditPage from "../components/EditPage";
-import HelpPage from "../components/HelpPage";
-import CreatePage from "../components/CreatePage";
-import Header from "../components/Header";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import NotFoundPage from '../components/NotFoundPage';
+import Dashboard from '../components/Dashboard';
+import EditPage from '../components/EditPage';
+import HelpPage from '../components/HelpPage';
+import CreatePage from '../components/CreatePage';
+import Header from '../components/Header';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const AppRouter = () => {
   return (

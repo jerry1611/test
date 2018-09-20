e9410267835768d7b50d1f9dd96e868d85fb18c2
SreeTeja <Sree.Palli3@target.com>
2018-09-20 13:57:03 -0500

    Added switch case

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,11 +2,12 @@ import { createStore } from 'redux';
 
 const store = createStore((state = { count: 0 }, action) => {
   console.log('running');
-  if (action.type === 'INCREMENT') {
-    return {
-      count: state.count + 1
-    };
-  } else return state;
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
 });
 console.log(store.getState());
 store.dispatch({

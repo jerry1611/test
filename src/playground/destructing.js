088263922ffab60e3ede0c58cd9022322eb256c8
SreeTeja <Sree.Palli3@target.com>
2018-09-21 11:09:43 -0500

    Update

--- src/playground/destructing.js
+++ src/playground/destructing.js
@@ -3,8 +3,12 @@ const person = {
   Age: 8,
   location: {
     city: 'Hyderabad',
-    temp: 36
+    temp: 39
   }
 };
 
-console.log(`${person.name} is ${person.Age}`);
+const { name: firstName = 'Anonymous', Age } = person;
+
+console.log(`${firstName} is ${Age}`);
+const { city, temp: temperature } = person.location;
+console.log(`Its ${temperature} in ${city}`);

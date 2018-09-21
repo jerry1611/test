10586599f10073b4df690c20e5c7b4f1eb27dc8b
SreeTeja <Sree.Palli3@target.com>
2018-09-21 13:03:39 -0500

    test

--- src/playground/destructing.js
+++ src/playground/destructing.js
@@ -1,14 +1,24 @@
-const person = {
-  name: 'Teja',
-  Age: 8,
-  location: {
-    city: 'Hyderabad',
-    temp: 39
-  }
-};
+// const person = {
+//   name: 'Teja',
+//   Age: 8,
+//   location: {
+//     city: 'Hyderabad',
+//     temp: 39
+//   }
+// };
 
-const { name: firstName = 'Anonymous', Age } = person;
+// const { name: firstName = 'Anonymous', Age } = person;
 
-console.log(`${firstName} is ${Age}`);
-const { city, temp: temperature } = person.location;
-console.log(`Its ${temperature} in ${city}`);
+// console.log(`${firstName} is ${Age}`);
+// const { city, temp: temperature } = person.location;
+// console.log(`Its ${temperature} in ${city}`);
+
+const address = [
+  '1299 S Juniper Street',
+  'Philadelphia',
+  'Pensylvania',
+  '19147'
+];
+
+const [street, city = 'Houston', state = 'Texas'] = address;
+console.log(`you are in ${city} ${state}`);

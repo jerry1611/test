0fc54872cab3436eab8888dfe7807ceb31697024
SreeTeja <Sree.Palli3@target.com>
2018-09-24 09:30:25 -0500

    Added Reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,33 @@
 import { createStore, combineReducers } from 'redux';
 
+const expensesdefaultState = [];
+const expensesReducer = (state = expensesdefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+const filterdefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+const filterReducer = (state = filterdefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filterReducer
+  })
+);
+console.log(store.getState());
+
 const demoState = {
   expenses: [
     {

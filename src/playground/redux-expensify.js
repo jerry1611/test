b243f706e988a8cad5ed9945c2e4f1693da223e8
SreeTeja <Sree.Palli3@target.com>
2018-09-24 10:14:27 -0500

    Object destructuring

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,8 +1,26 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+const addExpense = ({
+  description = '',
+  note = '',
+  amount = 0,
+  createdAt = 0
+} = {}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
 
 const expensesdefaultState = [];
 const expensesReducer = (state = expensesdefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
     default:
       return state;
   }
@@ -26,7 +44,12 @@ const store = createStore(
     filters: filterReducer
   })
 );
-console.log(store.getState());
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
 
 const demoState = {
   expenses: [
